c1a0074bbdb8a51bcf7b20e3706d25f24b171cc8
Diego Felipe Jaramillo Alvarez <dief.jara@hotmail.com>
2025-03-14 23:15:13 -0500

    se crea componente create-user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from "./login/login.component";
 import { ForgotPaswordComponent } from "./forgot-pasword/forgot-pasword.component";
+import { NewPasswordComponent } from './new-password/new-password.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent,ForgotPaswordComponent],
+  imports: [LoginComponent,ForgotPaswordComponent,NewPasswordComponent, CreateUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
